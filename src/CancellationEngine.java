07668dbed2448dc52e33df660769e7bae77579ee
adibaalya <164612947+adibaalya@users.noreply.github.com>
2025-01-16 14:50:27 +0800

    modularize the CancellationEngine class

--- src/CancellationEngine.java
+++ src/CancellationEngine.java
@@ -8,41 +8,56 @@ public class CancellationEngine {
 	static Scanner in = new Scanner(System.in);
 	PaymentEngine pEngine;
 	
-	void cancelTicket(User user)
-	{
-		
-		System.out.println("******************EXISTING BOOKINGS*****************");
+	// Main method to cancel ticket
+    void cancelTicket(User user) {
+        displayExistingBookings(user);
+        int bookingNumber = getBookingNumberToCancel(user);
+        
+        if (isValidBooking(user, bookingNumber)) {
+            processCancellation(user, bookingNumber);
+        } else {
+            System.out.println("Please Enter a valid booking Number");
+        }
+    }
 
-		System.out.println("The User has the following bookings\n");
-		HashMap<Integer,Ticket> bookings = user.getBookings();
-		
-		for(Ticket ticket : bookings.values())
-		{
-			System.out.println("Booking Number: "+ ticket.getBookingNumber());
-			System.out.println(" From : "+ticket.getStartCity());
-			System.out.println(" To : "+ticket.getEndCity());
-		}
-		System.out.println("****************************************");
+    // Method to display existing bookings
+    private void displayExistingBookings(User user) {
+        System.out.println("******************EXISTING BOOKINGS*****************");
+        System.out.println("The User has the following bookings\n");
+        HashMap<Integer, Ticket> bookings = user.getBookings();
+        
+        for (Ticket ticket : bookings.values()) {
+            System.out.println("Booking Number: " + ticket.getBookingNumber());
+            System.out.println(" From : " + ticket.getStartCity());
+            System.out.println(" To : " + ticket.getEndCity());
+        }
+        System.out.println("****************************************");
+    }
 
-		System.out.println("Please Enter the booking Number you wish to cancel");
-		int bookingNumber = in.nextInt();
-		if(bookings.containsKey(bookingNumber))
-		{
-			Ticket t = bookings.get(bookingNumber);
-			System.out.println("Your booking has been cancelled");
-			System.out.println("Amount of "+t.getFare()+" has been credited back to your account");
-			if(pEngine == null)
-				pEngine = new PaymentEngine();
-			
-			pEngine.processRefund(user,t);
-			user.removeBooking(bookingNumber);
-			System.out.println("*****************************************");
+    // Method to prompt user for booking number and validate
+    private int getBookingNumberToCancel(User user) {
+        System.out.println("Please Enter the booking Number you wish to cancel");
+        return in.nextInt();
+    }
 
-		}
-		else
-		{
-			System.out.println("Please Enter a valid booking Number");
-		}
-	}
+    // Method to check if the booking number exists in user's bookings
+    private boolean isValidBooking(User user, int bookingNumber) {
+        HashMap<Integer, Ticket> bookings = user.getBookings();
+        return bookings.containsKey(bookingNumber);
+    }
+
+    // Method to process the cancellation and refund
+    private void processCancellation(User user, int bookingNumber) {
+        Ticket ticket = user.getBookings().get(bookingNumber);
+        System.out.println("Your booking has been cancelled");
+        System.out.println("Amount of " + ticket.getFare() + " has been credited back to your account");
+
+        if (pEngine == null) {
+            pEngine = new PaymentEngine();
+        }
+        pEngine.processRefund(user, ticket);
+        user.removeBooking(bookingNumber);
+        System.out.println("*****************************************");
+    }
 
 }
